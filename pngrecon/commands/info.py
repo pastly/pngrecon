07e6de6ce98fec29515ca30e896d468a40f55c9f
Matt Traudt <sirmatt@ksu.edu>
2018-02-26 15:00:39 -0500

    Change format of info command

--- pngrecon/commands/info.py
+++ pngrecon/commands/info.py
@@ -1,4 +1,5 @@
 from ..lib.chunk import read_image_stream
+from ..lib.chunk import ChunkType
 from ..util.log import log
 from argparse import ArgumentDefaultsHelpFormatter
 
@@ -13,4 +14,17 @@ def main(args):
         chunks = read_image_stream(fd)
     log(args.image, 'contains', len(chunks), 'chunks')
     for c in chunks:
-        log('Chunk', c.type, 'with len', c.length)
+        try:
+            chunk_type = ChunkType(c.type)
+        except ValueError:
+            chunk_type = None
+        if chunk_type is not None:
+            if chunk_type == ChunkType.Index:
+                chunk_type = 'IndexChunk'
+            elif chunk_type == ChunkType.Data:
+                chunk_type = 'DataChunk'
+            else:
+                chunk_type = 'Chunk {} (pngrecon???)'.format(c.type)
+        else:
+            chunk_type = 'Chunk {}'.format(c.type)
+        log(chunk_type, 'with len', c.length)
