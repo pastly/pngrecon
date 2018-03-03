fb64b25e7b14d45c12346e1a56fb3090c070b75b
Matt Traudt <sirmatt@ksu.edu>
2018-03-03 16:31:02 -0500

    Be able to log info about multiple files

--- pngrecon/commands/info.py
+++ pngrecon/commands/info.py
@@ -9,7 +9,7 @@ import os
 
 def gen_parser(sub_p):
     p = sub_p.add_parser('info', formatter_class=ArgumentDefaultsHelpFormatter)
-    p.add_argument('image', nargs='?', default='/dev/stdin')
+    p.add_argument('image', nargs='*', default='/dev/stdin')
 
 
 def get_chunk_extra_info_index(chunk):
@@ -48,29 +48,34 @@ def get_chunk_extra_info(chunk):
 
 
 def main(args):
-    if not os.path.exists(args.image):
-        fail_hard(args.image, 'must exist')
-    if os.path.isdir(args.image):
-        fail_hard('Image can\'t be a directory')
-    with open(args.image, 'rb') as fd:
-        chunks = read_image_stream(fd)
-    if chunks is None:
-        fail_hard(args.image, 'does not appear to be a PNG')
-    log(args.image, 'contains', len(chunks), 'chunks')
-    for c in chunks:
-        try:
-            chunk_type = ChunkType(c.type)
-            if chunk_type == ChunkType.Index:
-                c = IndexChunk.from_chunk(c)
-            elif chunk_type == ChunkType.CryptInfo:
-                c = CryptInfoChunk.from_chunk(c)
-            elif chunk_type == ChunkType.Data:
-                c = DataChunk.from_chunk(c)
-        except ValueError:
-            chunk_type = None
-        if chunk_type is None:
-            chunk_type = 'Chunk {}'.format(c.type)
-        valid = '' if c.is_valid else '(INVALID)'
-        log(chunk_type, 'with len', c.length, valid)
-        for line in get_chunk_extra_info(c):
-            log('   ', line)
+    if not isinstance(args.image, list):
+        args.image = [args.image]
+    for image in args.image:
+        if not os.path.exists(image):
+            log(image, 'doesn\'t exist, so skipping.')
+            continue
+        if os.path.isdir(image):
+            log(image, 'is a directory, so skipping.')
+            continue
+        with open(image, 'rb') as fd:
+            chunks = read_image_stream(fd)
+        if chunks is None:
+            fail_hard(image, 'does not appear to be a PNG')
+        log(image, 'contains', len(chunks), 'chunks')
+        for c in chunks:
+            try:
+                chunk_type = ChunkType(c.type)
+                if chunk_type == ChunkType.Index:
+                    c = IndexChunk.from_chunk(c)
+                elif chunk_type == ChunkType.CryptInfo:
+                    c = CryptInfoChunk.from_chunk(c)
+                elif chunk_type == ChunkType.Data:
+                    c = DataChunk.from_chunk(c)
+            except ValueError:
+                chunk_type = None
+            if chunk_type is None:
+                chunk_type = 'Chunk {}'.format(c.type)
+            valid = '' if c.is_valid else '(INVALID)'
+            log(chunk_type, 'with len', c.length, valid)
+            for line in get_chunk_extra_info(c):
+                log('   ', line)
