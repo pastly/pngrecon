f97155e08614e1cafcc16ee98ddecdcd5d38eee7
Matt Traudt <sirmatt@ksu.edu>
2018-03-04 21:50:58 -0500

    Bump version; almost done

--- pngrecon/__main__.py
+++ pngrecon/__main__.py
@@ -4,7 +4,7 @@ import pngrecon.commands.decode
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
-PNG_RECON_VERSION = '0.0.2'
+PNG_RECON_VERSION = '0.1.0'
 
 
 def create_parser():
