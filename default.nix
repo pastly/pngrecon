85f023c8484071e97f44f7a79a23bd93d75bc5ba
Matt Traudt <sirmatt@ksu.edu>
2023-09-07 19:29:22 -0500

    default.nix bump 0.2.1

--- default.nix
+++ default.nix
@@ -3,12 +3,12 @@ with import <nixpkgs> {};
 
 pkgs.python311Packages.buildPythonPackage rec {
   pname = "pngrecon";
-  version = "0.2.0";
+  version = "0.2.1";
   src = fetchFromGitHub {
     owner = "pastly";
     repo = "pngrecon";
-    rev = "ad112d395d7fd6b3d50a446721adaca721fdc812";
-    hash = "sha256-CGfEWaq5TjBghTwAxhOG54/4i+eyDrUlxpVdc6XH45U=";
+    rev = "443f33c1213784a8fe755998581e01e39d6ff1a4";
+    hash = "sha256-kf828v1YjTdWKRRU0/1+ujBC64d2LA81JCal2Z/FJI4=";
   };
   meta = with lib; {
     # not sure if tghese variables in the link will actually work
